f63e1f44e4d0fdc7b0b3a0252854453a7a43d6a0
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-09-11 20:41:36 -0300

    decimo requisito

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const setColors = ['rgb(0, 0, 0)', 'rgb(35, 92, 68)', 'rgb(109, 41, 117)', 'rgb(117, 13, 16)'];
+const pixelBoard = document.querySelector('#pixel-board');
+let = lineColum = 5;
 
+// Cria a paleta de cores e preeche o backgroud com cada cor.
 function createPaletteColors() {
   const paletteColor = document.querySelector('#color-palette');
   for (let index = 0; index < setColors.length; index += 1) {
@@ -8,13 +11,16 @@ function createPaletteColors() {
     createListColor.classList.add('color');
     paletteColor.appendChild(createListColor);
   }
+  // Define a primeira paleta da cor Preta como Selecionada.
+  const selectPalette = document.querySelectorAll('.color');
+  selectPalette[0].classList.add('selected');
 }
 
+// Chama a função para criar a paleta de cores.
 createPaletteColors();
 
-function createLine() {
-  const lineColum = 5;
-  const pixelBoard = document.querySelector('#pixel-board');
+// Função que cria linha do box usando uma lista.
+function createLine(lineColum) {
   for (let index = 0; index < lineColum; index += 1) {
     const createLineBoard = document.createElement('li');
     createLineBoard.style.backgroundColor = 'white';
@@ -23,18 +29,44 @@ function createLine() {
   }
 }
 
-function createColum() {
-  const lineColum = 5;
-  const pixelBoard = document.querySelector('#pixel-board');
+// Função que cria colunas do box usando listas.
+function createColum(lineColum) {
   for (let index = 0; index < lineColum; index += 1) {
     const createColumBoard = document.createElement('ul');
     createColumBoard.classList.add('colum-pixel');
     pixelBoard.appendChild(createColumBoard);
-    createLine();
+    // Chama a função cria linha definindo o parametro de quantidade de linhas a serem criadas.
+    createLine(lineColum);
   }
 }
 
-createColum();
+// Chama a função para criar o box com valor inicial 5*5.
+createColum(lineColum);
+
+// Função para remover todas as linhas do box.
+function removeLine() {
+  const selectAllLine = document.querySelectorAll('.pixel');
+  for (let index = 0; index < selectAllLine.length; index += 1) {
+    pixelBoard.removeChild(selectAllLine[index]);
+  }
+}
+
+// Função para remover todas as colunas do box.
+function removeColum() {
+  const selectAllColum = document.querySelectorAll('.colum-pixel');
+  for (let index = 0; index < selectAllColum.length; index += 1) {
+    pixelBoard.removeChild(selectAllColum[index]);
+  }
+}
+
+//  Função que ao clicar recebe a cor da palheta selecionada.
+const selectPixels = document.querySelectorAll('.pixel');
+for (let index = 0; index < selectPixels.length; index += 1) {
+  selectPixels[index].addEventListener('click', () => {
+    const pixelColor = document.querySelector('.selected').style.backgroundColor;
+    selectPixels[index].style.backgroundColor = pixelColor;
+  });
+}
 
 const selectPalette = document.querySelectorAll('.color');
 selectPalette[0].classList.add('selected');
@@ -47,16 +79,8 @@ for (let index = 0; index < selectPalette.length; index += 1) {
   });
 }
 
-const selectPixels = document.querySelectorAll('.pixel');
-for (let index = 0; index < selectPixels.length; index += 1) {
-  selectPixels[index].addEventListener('click', () => {
-    const pixelColor = document.querySelector('.selected').style.backgroundColor;
-    selectPixels[index].style.backgroundColor = pixelColor;
-  });
-}
-
-const selectButton = document.querySelector('#clear-board');
-selectButton.addEventListener('click', () => {
+const selectButtonClear = document.querySelector('#clear-board');
+selectButtonClear.addEventListener('click', () => {
   const colorSelect = document.querySelector('.selected').style.backgroundColor;
   for (let index = 0; index < selectPixels.length; index += 1) {
     if (selectPixels[index].style.backgroundColor === colorSelect) {
@@ -64,3 +88,17 @@ selectButton.addEventListener('click', () => {
     }
   }
 });
+
+const inputText = document.querySelector('#board-size');
+const selectButtonBoard = document.querySelector('#generate-board');
+selectButtonBoard.addEventListener('click', () => {
+  if (inputText.value === '') {
+    return alert('Board inválido!');
+  }
+  removeLine();
+  removeColum();
+  lineColum = inputText.value;
+  createColum(lineColum);
+});
+
+const teste = document.querySelectorAll('.pixel');
